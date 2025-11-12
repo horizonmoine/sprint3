33a37be20f9a965b0b8111973e9b1c0289beb227
None None <marko.lecomtekolal@monlycee.net>
2025-11-12 16:53:45 +0100

    1er push y'a tout normalement

---
+++ PharmaSI_S2/Alchifra/ConnexionSql.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+
+namespace Alchifra 
+{
+    public class ConnexionSql
+    {
+        private static ConnexionSql connexion = null;
+        private MySqlConnection cnx;
+        private string connString;
+
+        // Constructeur privé car appelé par getInstance
+        private ConnexionSql(string unProvider, string uneDataBase, string unUid, string unMdp)
+        {
+            Initialize(unProvider, uneDataBase, unUid, unMdp);
+        }
+
+        //Initialisation de la connexion avec les paramètres de connexion
+        private void Initialize(string unProvider, string uneDataBase, string unUid, string unMdp)
+        {
+            try
+            {
+                connString = $"SERVER={unProvider};DATABASE={uneDataBase};UID={unUid};PASSWORD={unMdp}";
+                cnx = new MySqlConnection(connString);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+        //Méthode de création d'une instance de connexion si elle n'existe pas (singleton)
+        public static ConnexionSql GetInstance(string unProvider, string uneDataBase, string unUid, string unMdp)
+        {
+            try
+            {
+                if (connexion == null)
+                {
+                    // Premier appel
+                    connexion = new ConnexionSql(unProvider, uneDataBase, unUid, unMdp);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Erreur lors de la création de la connexion : " + e.Message,
+                                "Erreur de connexion",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+            }
+
+            return connexion;
+        }
+
+
+        //Récupération de la connexion
+        public MySqlConnection GetConnexion()
+        {
+            return this.cnx;
+        }
+
+        //Ouverture de la connexion
+        public void OpenConnexion()
+        {
+            try
+            {
+                cnx.Open();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        //Fermeture de la connexion
+        public void CloseConnexion()
+        {
+            cnx.Close();
+        }
+
+        //Exécutiuon d'une requête
+        public MySqlCommand ReqExec(string req)
+        {
+            MySqlCommand mysqlCmd = new MySqlCommand(req, this.cnx);
+            return mysqlCmd;
+        }
+    }
+}
+
+
+
---
+++ PharmaSI_S2/Alchifra/FormVisiteur.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Alchifra
+{
+    partial class FormVisiteur
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.btnRapports = new System.Windows.Forms.Button();
+            this.btnSaisie = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnLogOut = new System.Windows.Forms.Button();
+            this.btnPracticiens = new System.Windows.Forms.Button();
+            this.btnProduits = new System.Windows.Forms.Button();
+            this.btnAct = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.BackColor = System.Drawing.Color.SeaGreen;
+            this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.Location = new System.Drawing.Point(352, 280);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 29);
+            this.lblMessage.TabIndex = 2;
+            // 
+            // btnRapports
+            // 
+            this.btnRapports.Location = new System.Drawing.Point(47, 147);
+            this.btnRapports.Name = "btnRapports";
+            this.btnRapports.Size = new System.Drawing.Size(126, 42);
+            this.btnRapports.TabIndex = 4;
+            this.btnRapports.Text = "Rapports";
+            this.btnRapports.UseVisualStyleBackColor = true;
+            // 
+            // btnSaisie
+            // 
+            this.btnSaisie.Location = new System.Drawing.Point(47, 227);
+            this.btnSaisie.Name = "btnSaisie";
+            this.btnSaisie.Size = new System.Drawing.Size(126, 42);
+            this.btnSaisie.TabIndex = 5;
+            this.btnSaisie.Text = "Saisie";
+            this.btnSaisie.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.ErrorImage = global::Alchifra.Properties.Resources.compte;
+            this.pictureBox3.Image = global::Alchifra.Properties.Resources.compte;
+            this.pictureBox3.Location = new System.Drawing.Point(56, 33);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(117, 84);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox2.ErrorImage = global::Alchifra.Properties.Resources.profil2;
+            this.pictureBox2.Image = global::Alchifra.Properties.Resources.profil2;
+            this.pictureBox2.Location = new System.Drawing.Point(-25, -25);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(272, 688);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.ErrorImage = global::Alchifra.Properties.Resources.logo;
+            this.pictureBox1.Image = global::Alchifra.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(952, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(123, 112);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnLogOut
+            // 
+            this.btnLogOut.Image = global::Alchifra.Properties.Resources.deco3;
+            this.btnLogOut.Location = new System.Drawing.Point(81, 557);
+            this.btnLogOut.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnLogOut.Name = "btnLogOut";
+            this.btnLogOut.Size = new System.Drawing.Size(54, 54);
+            this.btnLogOut.TabIndex = 9;
+            this.btnLogOut.UseVisualStyleBackColor = true;
+            // 
+            // btnPracticiens
+            // 
+            this.btnPracticiens.Location = new System.Drawing.Point(47, 395);
+            this.btnPracticiens.Name = "btnPracticiens";
+            this.btnPracticiens.Size = new System.Drawing.Size(126, 42);
+            this.btnPracticiens.TabIndex = 10;
+            this.btnPracticiens.Text = "Praticiens";
+            this.btnPracticiens.UseVisualStyleBackColor = true;
+            // 
+            // btnProduits
+            // 
+            this.btnProduits.Location = new System.Drawing.Point(47, 478);
+            this.btnProduits.Name = "btnProduits";
+            this.btnProduits.Size = new System.Drawing.Size(126, 42);
+            this.btnProduits.TabIndex = 11;
+            this.btnProduits.Text = "Produits";
+            this.btnProduits.UseVisualStyleBackColor = true;
+            // 
+            // btnAct
+            // 
+            this.btnAct.Location = new System.Drawing.Point(47, 314);
+            this.btnAct.Name = "btnAct";
+            this.btnAct.Size = new System.Drawing.Size(126, 41);
+            this.btnAct.TabIndex = 12;
+            this.btnAct.Text = "Activité ";
+            this.btnAct.UseVisualStyleBackColor = true;
+            // 
+            // FormVisiteur
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaGreen;
+            this.ClientSize = new System.Drawing.Size(1101, 654);
+            this.Controls.Add(this.btnAct);
+            this.Controls.Add(this.btnProduits);
+            this.Controls.Add(this.btnPracticiens);
+            this.Controls.Add(this.btnLogOut);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.btnSaisie);
+            this.Controls.Add(this.btnRapports);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "FormVisiteur";
+            this.Text = "FormVisiteur";
+            this.Load += new System.EventHandler(this.FormVisiteur_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnRapports;
+        private System.Windows.Forms.Button btnSaisie;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button btnLogOut;
+        private System.Windows.Forms.Button btnPracticiens;
+        private System.Windows.Forms.Button btnProduits;
+        private System.Windows.Forms.Button btnAct;
+    }
+}
